3787bf1a669b47e4632b6d9d5a8d08ecd4151b85
Dmytro <dmazepa@speroteck.com>
2012-11-20 17:16:23 +0200

    first

---
+++ src/test/java/thucydides/jbehave/JBehaveTestCase.groovy
@@ -0,0 +1,7 @@
+package thucydides.jbehave
+
+import net.thucydides.jbehave.ThucydidesJUnitStories;
+
+public class JBehaveTestCase extends ThucydidesJUnitStories {
+    public JBehaveTestCase() {}
+}
