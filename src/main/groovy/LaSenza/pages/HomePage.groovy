3b7c01fb2ca0f22ca3bc547ad7a5dcb0c51fccd7
Dmytro <dmazepa@speroteck.com>
2012-11-22 20:56:55 +0200

    Config url

--- src/main/groovy/LaSenza/pages/HomePage.groovy
+++ src/main/groovy/LaSenza/pages/HomePage.groovy
@@ -1,10 +1,8 @@
 package LaSenza.pages
 
-import net.thucydides.core.annotations.DefaultUrl
-import org.openqa.selenium.WebDriver
 import net.thucydides.core.pages.PageObject
+import org.openqa.selenium.WebDriver
 
-@DefaultUrl("http://localhost:9000")
 class HomePage extends PageObject{
 
     HomePage(WebDriver driver){
