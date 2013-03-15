f8365bdf830d474ec3b0d4519598bb13dbc04385
Dmytro <dmazepa@speroteck.com>
2013-03-15 15:19:01 +0200

    images on pdp

--- src/test/groovy/LaSenza/CheckoutConfirmAndPay.groovy
+++ src/test/groovy/LaSenza/CheckoutConfirmAndPay.groovy
@@ -2,6 +2,5 @@ package LaSenza
 
 import net.thucydides.jbehave.ThucydidesJUnitStory
 
-class SecondStep extends ThucydidesJUnitStory {
-
+class CheckoutConfirmAndPay extends ThucydidesJUnitStory{
 }
